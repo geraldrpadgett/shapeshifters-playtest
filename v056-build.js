3b34562cde4ebd78698d299b1aaf86d27ae5ea00
geraldrpadgett <geraldrpadgett@gmail.com>
2026-09-02 11:09:26 -0400

    Build v0.5.6 DnD tabletop theme

---
+++ v056-build.js
@@ -0,0 +1,12 @@
+const fs=require('fs');
+const path=require('path');
+const out=path.join(__dirname,'dist');
+for(const name of ['shifters-v056-dnd.css','shifters-v056-dnd.js'])fs.copyFileSync(path.join(__dirname,name),path.join(out,name));
+const indexPath=path.join(out,'index.html');
+let html=fs.readFileSync(indexPath,'utf8');
+html=html.replace(/\s*<link[^>]*shifters-v056-dnd\.css[^>]*>/ig,'');
+html=html.replace(/\s*<script[^>]*shifters-v056-dnd\.js[^>]*><\/script>/ig,'');
+html=html.replace('</head>','  <link rel="stylesheet" href="shifters-v056-dnd.css">\n</head>');
+html=html.replace('</body>','  <script src="shifters-v056-dnd.js"></script>\n</body>');
+fs.writeFileSync(indexPath,html);
+console.log('Applied Shapeshifters v0.5.6 D&D tabletop overhaul.');
