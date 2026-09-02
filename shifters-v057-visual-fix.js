f981956179c0facacf4fa7b8c9c0f8099312f72b
geraldrpadgett <geraldrpadgett@gmail.com>
2026-09-02 11:28:44 -0400

    Tag v0.5.7 visual layering fix

---
+++ shifters-v057-visual-fix.js
@@ -0,0 +1,5 @@
+(function(){
+  'use strict';
+  if(typeof GAME_DATA!=='undefined')GAME_DATA.version='0.5.7-visual-layer-fix';
+  console.info('Shapeshifters v0.5.7 visual layering fix active');
+})();
